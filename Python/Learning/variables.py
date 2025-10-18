41957ac26f7dcdda13528e7ca3e00105036c6e48
DominusDev658 <antonjaredcastillo@gmail.com>
2025-10-18 14:53:46 -0700

    Finished a small project

---
+++ Python/Learning/variables.py
@@ -0,0 +1,13 @@
+import random
+
+x = int(5)
+y = str("Hello World")
+
+print(x)
+print(y)
+
+print(type(x))
+print(type(y))
+
+z = random.randrange(1,10)
+print(z)
---
+++ Python/Mini Projects/madlibs.py
@@ -0,0 +1,9 @@
+noun1 = input("Enter a noun: ")
+verb1 = input("Enter a verb: ")
+adverb1 = input("Enter an adverb: ")
+adjective1 = input("Enter an adjective: ")
+gender = input("Enter a gender: ")
+
+print(f"{noun1} was a {gender}")
+print(f"{noun1} was {verb1} {adverb1}")
+print(f"{noun1} was very {adjective1}")
